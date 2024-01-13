5360abc5faa772e707c6375bc48f1ad74481b609
E B <embosi97@gmail.com>
2024-01-13 16:30:07 -0500

    Formatting

--- oddsPickr/src/main/java/com/projects/oddsPickr/config/OpenAPIConfig.java
+++ oddsPickr/src/main/java/com/projects/oddsPickr/config/OpenAPIConfig.java
@@ -1,12 +1,9 @@
 package com.projects.oddsPickr.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.servers.Server;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -25,27 +22,5 @@ import org.springframework.web.bind.annotation.GetMapping;
         )
 )
 public class OpenAPIConfig {
-
-    @Operation(
-            description = "Get Event(s) by Sport/Event ID",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "Success"
-                    ),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Not Found: Invalid Sport, Region, or Event ID"
-                    ),
-                    @ApiResponse(
-                            responseCode = "403",
-                            description = "Unauthorized: Invalid URL or permission"
-                    )
-            }
-    )
-    @GetMapping
-    public String get() {
-        return "Get Mapping";
-    }
 
 }
